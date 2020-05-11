892299f9c69fdb97ba9f8d2b9ffabce842889160
Quentin <127526@student.drenthecollege.nl>
2020-05-11 13:00:03 +0200

    toetsenbord werkt met het randomwoord

--- Index.js
+++ Index.js
@@ -1,3 +1,12 @@
 $(".LetterB").click(function() {
-    console.log($(this).text());
+    var letter = $(this).text();
+    onguess(letter)
 });
+
+function onguess(letter) {
+    for (i = 0; i < randomwoord.length; i++) {
+        if (randomwoord.charAt(i).toUpperCase() == letter.toUpperCase()) {
+            document.getElementById("letter_" + i).innerHTML = letter;
+        }
+    }
+};
--- woordgen.js
+++ woordgen.js
@@ -1,20 +1,21 @@
 var woordarray = ["huis", "hond", "toetsenbord", "asfalt", "cola", "bankpas", "computer", "tuin", "bloem", "pan",
- "telefoon", "oplader", "toetje", "cd", "ijsje", "lamp", "baby", "muziek", "gieter", "elastiekje",
-  "bed", "gas",  "auto", "nintendo", "bord", "water", "papier", "glas", "plant", "ganzenbord", ];
+    "telefoon", "oplader", "toetje", "cd", "ijsje", "lamp", "baby", "muziek", "gieter", "elastiekje",
+    "bed", "gas", "auto", "nintendo", "bord", "water", "papier", "glas", "plant", "ganzenbord",
+];
 var randomwoord;
 
-function pickRandom(){
-  randomwoord = woordarray[Math.floor(Math.random() * woordarray.length)];
-  printRandom();
+function pickRandom() {
+    randomwoord = woordarray[Math.floor(Math.random() * woordarray.length)];
+    printRandom();
 }
 
-function printRandom(){
-  for (var i = 0; i < randomwoord.length; i++) {
-    var parent = document.getElementById("word");
-    var newEle = document.createElement('div');
-    newEle.setAttribute("id", "letter_" + i);
-    newEle.setAttribute("class", "letterdiv");
-    parent.appendChild(newEle);
-  }
+function printRandom() {
+    for (var i = 0; i < randomwoord.length; i++) {
+        var parent = document.getElementById("word");
+        var newEle = document.createElement('div');
+        newEle.setAttribute("id", "letter_" + i);
+        newEle.setAttribute("class", "letterdiv");
+        parent.appendChild(newEle);
+    }
 
 }

f32fbbfef4a5568a77bad04ed53cb07472742c1d
Jaimy Tieck <127511@student.drenthecollege.nl>
2020-05-11 12:13:07 +0200

    Woorden generator gemaakt.

---
+++ woordgen.js
@@ -0,0 +1,20 @@
+var woordarray = ["huis", "hond", "toetsenbord", "asfalt", "cola", "bankpas", "computer", "tuin", "bloem", "pan",
+ "telefoon", "oplader", "toetje", "cd", "ijsje", "lamp", "baby", "muziek", "gieter", "elastiekje",
+  "bed", "gas",  "auto", "nintendo", "bord", "water", "papier", "glas", "plant", "ganzenbord", ];
+var randomwoord;
+
+function pickRandom(){
+  randomwoord = woordarray[Math.floor(Math.random() * woordarray.length)];
+  printRandom();
+}
+
+function printRandom(){
+  for (var i = 0; i < randomwoord.length; i++) {
+    var parent = document.getElementById("word");
+    var newEle = document.createElement('div');
+    newEle.setAttribute("id", "letter_" + i);
+    newEle.setAttribute("class", "letterdiv");
+    parent.appendChild(newEle);
+  }
+
+}
